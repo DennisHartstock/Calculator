9a6c35870b39e2192903ebdaf184ede348796939
DennsHartstock <dennis.hartstock@web.de>
2022-11-15 08:36:45 +0100

    add business logic

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvExample = findViewById(R.id.tvExample);
+        EditText etAnswer = findViewById(R.id.etAnswer);
+        Button btAnswer = findViewById(R.id.btAnswer);
+        TextView tvWrongAnswer = findViewById(R.id.tvWrongAnswer);
+        TextView tvRightAnswer = findViewById(R.id.tvRightAnswer);
+
+        tvExample.setText("20 + 5 = ?");
+
+        btAnswer.setOnClickListener(view -> {
+            String answer = etAnswer.getText().toString();
+
+            if (!answer.equals("") & answer.equals("25")) {
+                tvRightAnswer.setVisibility(View.VISIBLE);
+                tvWrongAnswer.setVisibility(View.GONE);
+            } else {
+                tvRightAnswer.setVisibility(View.GONE);
+                tvWrongAnswer.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
